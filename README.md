2b598ca5495d419a2150df9e1e3ec282ac9c0d8d
asrul10 <asrul@kotaxdev.com>
2023-08-07 09:32:43 +0700

    fix: set password to env

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ### Requirements
 - [aws cli](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html)
+- set password to environment variable `export MYSQL_PWD=your_password`
--- backup.sh
+++ backup.sh
@@ -2,13 +2,12 @@
 
 # set your database credentials here
 username="username"
-password="password"
 database="database"
 db_dump_prefix="db_dump"
 bucket_name="bucket_name"
 
 now=$(date +%F-%H:%M)
 db_name="${db_dump_prefix}_${now}.sql.gz"
-mysqldump -u ${username} ${database} -p"${password}" --no-tablespaces | gzip > "$db_name"
+mysqldump -u ${username} ${database} --no-tablespaces | gzip > "$db_name"
 aws s3 cp "$db_name" "s3://${bucket_name}/${db_name}"
 rm "$db_name"
